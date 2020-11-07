ae825b7f67caee28eeccdc2da485c404846b5f52
SPradeesh <Waterme1!>
2020-11-08 00:39:18 +0100

    Initial check from Intellij

---
+++ FlowControl/src/com/oops/StartKit.java
@@ -0,0 +1,15 @@
+package com.oops;
+
+public class StartKit {
+    public static void main (String args[])
+    {
+       // int[] age = new int[10];
+        int[] age = {14, 15, 4, 9};
+        System.out.println("Accessing the elements of array");
+        System.out.println("The first values is: " +age[0]);
+        System.out.println("The second array values is " +age[1]);
+        System.out.println("The third value in the array is " +age[2]);
+        System.out.println("The 4th value in the array is: " +age[3]);
+
+    }
+}
---
+++ FlowControl/src/com/oops/ZipThrough.java
@@ -0,0 +1,13 @@
+package com.oops;
+
+public class ZipThrough{
+    public static void main(String args[]) {
+
+
+        int[] age = {14, 15, 4, 9};
+
+        for (int i = 0; i < age.length; ++i) {
+            System.out.println("Hello array list" + age[i]);
+        }
+    }
+}
