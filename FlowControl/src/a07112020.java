ede9df032bad736078b11e8aeec7ca274bd32a9a
SPradeesh <Waterme1!>
2020-11-07 01:03:29 +0100

    Initial check from Intellij

--- FlowControl/src/a07112020.java
+++ FlowControl/src/a07112020.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class a07112020 {
+
     public static void main(String[] args) {
-        System.out.println("Hello WorldS");
+
+        Double number, sum = 0.0;
+        // create an object of Scanner
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 1; i < 6; ++i) {
+            System.out.print("Enter number " + i + " : ");
+            // takes input from the user
+            number = input.nextDouble();
+
+            // if number is negative
+            // continue statement is executed
+            if (number <= 0.0) {
+                continue;
+            }
+
+            sum += number;
+        }
+        System.out.println("Sum = " + sum);
+        input.close();
     }
 
 }
